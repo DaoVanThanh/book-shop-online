ef6cd8ae3fa734a8b720d2e1811a6dc89ed1d716
tunangoo <ngovantuan02092003@gmail.com>
2023-10-19 14:42:11 +0700

    update API GetBookDetail (API 12)

--- book-shop-server/src/main/java/com/example/bookshop/repository/BookRepository.java
+++ book-shop-server/src/main/java/com/example/bookshop/repository/BookRepository.java
@@ -5,17 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    @Query(
-            value = "select b from Book as b where b.bookId = :bookId"
-    )
-    Optional<Book> findByBookId(
-            @Param("bookId") Long bookId
-    );
-
+    Optional<Book> findByBookId(Long bookId);
 }

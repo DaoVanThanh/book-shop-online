bb939a9c91f0192c82e18d6a032b7a7fc6907901
milomolly <danglh@welearning.vn>
2023-10-24 20:59:08 +0700

    Update fe

--- book-shop-app/src/components/AppRoutes.jsx
+++ book-shop-app/src/components/AppRoutes.jsx
@@ -6,6 +6,8 @@ import Shop from "./Shop/Shop";
 import About from "./About/About";
 import Cart from "./Cart/Cart";
 import Policy from "./Policy/Policy";
+import Checkout from "./Checkout/Checkout";
+import Sidebar from "./Sidebar/Sidebar";
 
 function AppRoutes() {
   return (
@@ -16,6 +18,10 @@ function AppRoutes() {
         <Route path="/shop" element={<Shop />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
         <Route path="/policy" element={<Policy />}></Route>
+        <Route path="/checkout" element={<Checkout />}></Route>
+        <Route path="/sidebar" element={<Sidebar />}></Route>
+
+
     </Routes>
   );
 }

67d9dc11912ed3afce9f5fc9d0bc49a1124f96b4
Tienbruse <tien10a2cvp@gmail.com>
2023-11-02 04:48:20 +0700

    refreshToken + UI

--- book-shop-app/src/components/Login/Login.jsx
+++ book-shop-app/src/components/Login/Login.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
-import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCard, MDBCardBody, MDBInput, MDBCheckbox } from 'mdb-react-ui-kit';
+import {
+    MDBBtn,
+    MDBContainer,
+    MDBRow,
+    MDBCol,
+    MDBCard,
+    MDBCardBody,
+    MDBInput,
+    MDBCheckbox,
+    MDBIcon
+}
+    from 'mdb-react-ui-kit';
+
+import './styleLogin.css';
+
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 function Login() {
     const navigate = useNavigate();
@@ -21,23 +35,26 @@ function Login() {
     const handleLogin = () => {
         axios.post('http://localhost:8080/api/auth/authenticate', { username, password })
             .then((response) => {
-                if (response.data.accessToken) {
+                if (response.data.accessToken && response.data.refreshToken) {
                     setLoggedIn(true);
                     setLoginError(null);
 
                     const accessToken = response.data.accessToken;
+                    const refreshToken = response.data.refreshToken;
 
                     localStorage.setItem('accessToken', accessToken);
+                    localStorage.setItem('refreshToken', refreshToken);
 
-                    const storedToken = localStorage.getItem('accessToken');
+                    const storedAccessToken = localStorage.getItem('accessToken');
+                    const storedRefreshToken = localStorage.getItem('refreshToken');
 
-                    if (storedToken) {
-                        if (storedToken === response.data.accessToken) {
-                            alert("Đăng nhập thành công");
+                    if (storedAccessToken) {
+                        if (storedAccessToken === response.data.accessToken) {
+                           // alert("Đăng nhập thành công");
                             navigate("/Home");
                         }
                     } else {
-                        alert("Đăng nhập thất bại");
+                       // alert("Đăng nhập thất bại");
                     }
                 } else {
                     setLoggedIn(false);
@@ -47,46 +64,79 @@ function Login() {
             .catch((error) => {
                 setLoggedIn(false);
                 setLoginError("Đăng nhập thất bại");
-                alert("Đăng nhập thất bại");
             });
+        axios.interceptors.response.use(
+            response => response,
+            error => {
+                const originalRequest = error.config;
+                if (error.response.status === 401 && !originalRequest._retry) {
+                    originalRequest._retry = true;
+                    const refreshToken = localStorage.getItem('refreshToken');
+                    return axios.post('http://localhost:8080/api/auth/refreshToken', { refreshToken })
+                        .then(res => {
+                            if (res.status === 200) {
+                                localStorage.setItem('accessToken', res.data.accessToken);
+                                axios.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.accessToken;
+                                return axios(originalRequest);
+                            }
+                        });
+                }
+                return Promise.reject(error);
+            }
+        );
     };
-
     return (
-        <MDBContainer fluid>
-            <MDBRow className="d-flex justify-content-center align-items-center h-100">
-                <MDBCol col="12">
-                    <MDBCard className="bg-white my-5 mx-auto" style={{ borderRadius: '1rem', maxWidth: '500px' }}>
-                        <MDBCardBody className="p-5 w-100 d-flex flex-column">
-                            <h2 className="fw-bold mb-2 text-center">Đăng nhập</h2>
-                            <p className="text-white-50 mb-3">Vui lòng nhập tên đăng nhập và mật khẩu!</p>
-                            <MDBInput
-                                wrapperClass="mb-4 w-100"
-                                label="Tên đăng nhập"
-                                id="formControlLg"
-                                type="text"
-                                size="lg"
-                                value={username}
-                                onChange={handleUsernameChange}
-                            />
-                            <MDBInput
-                                wrapperClass="mb-4 w-100"
-                                label="Mật khẩu"
-                                id="formControlLg"
-                                type="password"
-                                size="lg"
-                                value={password}
-                                onChange={handlePasswordChange}
-                            />
-                            <MDBCheckbox name="flexCheck" id="flexCheckDefault" className="mb-4" label="Ghi nhớ mật khẩu" />
-                            <MDBBtn size="lg" style={{ width: '100%' }} onClick={handleLogin}>
-                                Đăng nhập
-                            </MDBBtn>
-                            {loginError && <p className="text-danger mt-3">{loginError}</p>}
-                            <hr className="my-4" />
+        <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
+
+            <MDBRow>
+
+                <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
+
+                    <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{color: 'hsl(218, 81%, 95%)'}}>
+                        BookStore <br />
+                        <span style={{color: 'hsl(218, 81%, 75%)'}}>Thả tâm hồn vào sách</span>
+                    </h1>
+
+                    <p className='px-3' style={{color: 'hsl(218, 81%, 85%)'}}>
+                        Đến với cửa hàng của chúng tôi, bạn không chỉ đến
+                        với nơi bán những cuốn sách chất lượng và uy tín,
+                        hãy cùng cảm nhận và hoà mình vào lý tưởng của tác giả
+                        trong những cuốn sách nổi tiếng, hay và tâm đắc nhất của cửa hàng
+                        chúng tôi.
+                    </p>
+
+                </MDBCol>
+
+                <MDBCol md='6' className='position-relative'>
+
+                    <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+                    <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
+
+                    <MDBCard className='my-5 bg-glass'>
+                        <MDBCardBody className='p-5'>
+
+                            <MDBInput wrapperClass='mb-4' id='form3' type='text' placeholder="Tên tài khoản" value={username} onChange={handleUsernameChange}/>
+
+                            <MDBInput wrapperClass='mb-4' id='form4' type='password' placeholder="Mật khẩu" value={password} onChange={handlePasswordChange}/>
+
+                            <div className='d-flex justify-content-center mb-4'>
+                                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Ghi nhớ mật khẩu' />
+                            </div>
+
+                            <MDBBtn className='w-100 mb-4' size='md' onClick={handleLogin}>Đăng nhập</MDBBtn>
+
+                            <div className="text-center">
+                                <p>Bạn chưa có tài khoản?</p>
+                                <Link to="/register">Đăng kí</Link>
+                            </div>
+
                         </MDBCardBody>
                     </MDBCard>
+
                 </MDBCol>
+
             </MDBRow>
+
         </MDBContainer>
     );
 }

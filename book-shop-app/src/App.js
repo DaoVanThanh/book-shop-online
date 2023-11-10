c34de766ff2e1f0e407409aad2c5e85c77a85ecd
DaoVanThanh <thanhap03@gmail.com>
2023-11-11 00:12:23 +0700

    fix cors

--- book-shop-app/src/App.js
+++ book-shop-app/src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 import React from "react";
 import AppRoutes from "./components/AppRoutes";
-import { BrowserRouter} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -11,12 +11,12 @@ import Admin from "./adminComponents/Admin";
 function App() {
   return (
     <BrowserRouter>
-      {/*<Header />
+      <Header />
       <div className="App">
         <AppRoutes />
       </div>
       <Footer />
-     {/*  <Admin /> */}
+      {/*  <Admin /> */}
     </BrowserRouter>
   );
 }
--- book-shop-server/src/main/java/com/example/bookshop/config/SecurityConfig.java
+++ book-shop-server/src/main/java/com/example/bookshop/config/SecurityConfig.java
@@ -9,6 +9,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -16,10 +21,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
-
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+
+        http.cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**")

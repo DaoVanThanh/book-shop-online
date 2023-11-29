ed48d4ced24ef575da49fdeceb0bb16613a06043
DaoVanThanh <thanhap03@gmail.com>
2023-11-29 23:19:05 +0700

    fix css

--- book-shop-app/src/adminComponents/ProductManagement/ProductManagement.jsx
+++ book-shop-app/src/adminComponents/ProductManagement/ProductManagement.jsx
@@ -517,8 +517,6 @@ const ProductManagement = () => {
               </Button>
             </div>
           </Form>
-          <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
-          <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
         </Modal.Body>
       </Modal>
     </div>

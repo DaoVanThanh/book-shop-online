c32be724abe72f60a491b6aa51d6bf1186e256ba
DaoVanThanh <thanhap03@gmail.com>
2023-11-30 23:18:54 +0700

    update: css

--- book-shop-app/src/components/ChangePassword/ChangePassword.jsx
+++ book-shop-app/src/components/ChangePassword/ChangePassword.jsx
@@ -1,80 +1,95 @@
-import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import {changePassword} from "../../apiServices/ChangePasswordService";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { changePassword } from "../../apiServices/ChangePasswordService";
+import { toast, ToastContainer } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 function ChangePassword() {
-    const [oldPassword, setOldPassword] = useState('');
-    const [newPassword, setNewPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [error, setError] = useState('');
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate()
 
-    const handlePasswordChange = async () => {
-        if (newPassword  !== confirmPassword) {
-            setError('Mật khẩu xác nhận không trùng khớp. Vui lòng nhập lại.');
-        } else if (!isPasswordValid(newPassword)) {
-            setError('Mật khẩu không đáp ứng yêu cầu an toàn. Vui lòng thử lại.');
-        } else {
-           await changePassword({ oldPassword, newPassword })
-                .then((response) => {
-                    // Xử lý kết quả từ API nếu cập nhật thành công
-                    console.log(response.data);
-                    alert(response.data);
-                    localStorage.clear();
-                })
-                .catch((error) => {
-                    // Xử lý lỗi từ API nếu có lỗi
-                    console.error(error);
-                    setError('Mật khẩu cũ không đúng. Vui lòng nhập lại');
-                });
-        }
-    };
+  const handlePasswordChange = async () => {
+    if (newPassword !== confirmPassword) {
+      setError("Mật khẩu xác nhận không trùng khớp. Vui lòng nhập lại.");
+    } else if (!isPasswordValid(newPassword)) {
+      setError("Mật khẩu không đáp ứng yêu cầu an toàn. Vui lòng thử lại.");
+    } else {
+      await changePassword({ oldPassword, newPassword })
+        .then((response) => {
+          // Xử lý kết quả từ API nếu cập nhật thành công
+          localStorage.clear();
+          navigate("/login")
+          toast("Đổi mật khẩu thành công. Vui lòng đăng nhập lại!");
+        })
+        .catch((error) => {
+          // Xử lý lỗi từ API nếu có lỗi
+          console.error(error);
+          setError("Mật khẩu cũ không đúng. Vui lòng nhập lại");
+        });
+    }
+  };
 
-    const isPasswordValid = (password) => {
-        // Sử dụng biểu thức chính quy (regex) để kiểm tra mật khẩu
-        const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-        return passwordRegex.test(password);
-    };
-    return (
-        <div className="d-flex justify-content-center" style={{ alignItems: "top" }}>
-            <Form style={{ width: "30%" }}>
-                <h1>Đổi Mật Khẩu</h1>
-                <Form.Group controlId="oldPassword" >
-                    <Form.Label className="mt-3">Mật khẩu cũ</Form.Label>
-                    <Form.Control
-                        type="password"
-                       // placeholder="Nhập mật khẩu cũ"
-                        value={oldPassword}
-                        onChange={(e) => setOldPassword(e.target.value)}
-                    />
-                </Form.Group>
+  const isPasswordValid = (password) => {
+    // Sử dụng biểu thức chính quy (regex) để kiểm tra mật khẩu
+    const passwordRegex =
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+  return (
+    <div
+      className="d-flex justify-content-center"
+      style={{ alignItems: "top" }}
+    >
+      <Form style={{ width: "30%" }}>
+        <h1>Đổi Mật Khẩu</h1>
+        <Form.Floating id="oldPassword" className="mb-3">
+          <Form.Control
+            id="old"
+            type="password"
+            placeholder="Nhập mật khẩu cũ"
+            value={oldPassword}
+            onChange={(e) => setOldPassword(e.target.value)}
+          />
+          <label htmlFor="old">Nhập mật khẩu cũ</label>
+        </Form.Floating>
 
-                <Form.Group controlId="newPassword">
-                    <Form.Label>Mật khẩu mới</Form.Label>
-                    <Form.Control
-                        type="password"
-                       // placeholder="Nhập mật khẩu mới"
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)}
-                    />
-                </Form.Group>
+        <Form.Floating id="newPassword" className="mb-3">
+          <Form.Control
+            id="new"
+            type="password"
+            placeholder="Nhập mật khẩu mới"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
+          <label htmlFor="new">Nhập mật khẩu mới</label>
+        </Form.Floating>
 
-                <Form.Group controlId="confirmPassword">
-                    <Form.Label className="mt-3">Xác nhận Mật khẩu mới</Form.Label>
-                    <Form.Control
-                        type="password"
-                     //   placeholder="Xác nhận mật khẩu mới"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                </Form.Group>
-                {error && <p className="text-danger">{error}</p>}
+        <Form.Floating id="confirmPassword" className="mb-3">
+          <Form.Control
+            id="confirm"
+            type="password"
+            placeholder="Xác nhận mật khẩu mới"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <label htmlFor="confirm">Xác nhận mật khẩu mới</label>
+        </Form.Floating>
+        {error && <p className="text-danger">{error}</p>}
 
-                <Button variant="primary" onClick={handlePasswordChange}   className="mt-3">
-                    Đổi Mật Khẩu
-                </Button>
-            </Form>
-        </div>
-    );
+        <Button
+          variant="primary"
+          onClick={handlePasswordChange}
+          className="mb-3"
+        >
+          Đổi Mật Khẩu
+        </Button>
+      </Form>
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default ChangePassword;
--- book-shop-app/src/components/InfoManagement/InfoManagement.jsx
+++ book-shop-app/src/components/InfoManagement/InfoManagement.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Row, Col, Tab, Nav } from "react-bootstrap";
 import { Outlet, NavLink } from "react-router-dom";
-import "./InfoManagement.css";
 
 const InfoManagement = () => {
+  const handleLogout = () => {
+    window.location.href = "/login";
+    localStorage.clear();
+  }
+
   return (
     <Tab.Container id="left-tabs-example">
       <Row>
         <Col sm={2}>
-          <Nav variant="pills" className="flex-column">
+          <Nav variant="pills" className="flex-column" style={{textAlign: "left"}}>
             <Nav.Item>
               <Nav.Link as={NavLink} to="/user/account">
                 Quản lý tài khoản
@@ -24,9 +28,12 @@ const InfoManagement = () => {
                 Đổi mật khẩu
               </Nav.Link>
             </Nav.Item>
+            <Nav.Item>
+              <Nav.Link onClick={handleLogout}>Đăng xuất</Nav.Link>
+            </Nav.Item>
           </Nav>
         </Col>
-        <Col sm={10}>
+        <Col sm={10} style={{padding: "0"}}>
           <Tab.Content>
             <Outlet />
           </Tab.Content>
